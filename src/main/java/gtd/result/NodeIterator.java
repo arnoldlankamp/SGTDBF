27012eec1d6e454683dae6a25a70afa27f76f4a6
Arnold Lankamp <alankamp@xebia.com>
2015-01-08 17:40:54 +0100

    -Added some useful result related utility classes.

---
+++ src/main/java/gtd/result/NodeIterator.java
@@ -0,0 +1,61 @@
+package gtd.result;
+
+import gtd.result.struct.Link;
+import gtd.util.ArrayList;
+
+import java.util.HashSet;
+
+@SuppressWarnings("unchecked")
+public abstract class NodeIterator{
+	private final AbstractNode result;
+	
+	public NodeIterator(AbstractNode result) {
+		super();
+		
+		this.result = result;
+	}
+	
+	protected abstract void visit(String name, SortContainerNode node);
+	
+	private void visitPrefix(Link link, HashSet<AbstractNode> visitedNodes) {
+		if(link == null) return;
+		
+		if(link.node instanceof AbstractContainerNode){
+			if(!visitContainerNode((AbstractContainerNode) link.node, visitedNodes)) return;
+		}
+		
+		if(link.prefixes != null) {
+			for(int i = link.prefixes.size() - 1; i >= 0; --i) {
+				visitPrefix(link.prefixes.get(i), visitedNodes);
+			}
+		}
+	}
+	
+	private boolean visitContainerNode(AbstractContainerNode node, HashSet<AbstractNode> visitedNodes) {
+		if(!visitedNodes.add(node)) return false;
+		
+		if(node instanceof SortContainerNode) {
+			visit(node.name, (SortContainerNode) node);
+		}
+		
+		if(node.alternatives instanceof Link) {
+			visitPrefix((Link) node.alternatives, visitedNodes);
+		} else {
+			ArrayList<Link> alternatives = (ArrayList<Link>) node.alternatives;
+			for(int i = alternatives.size() - 1; i >= 0; --i) {
+				visitPrefix(alternatives.get(i), visitedNodes);
+			}
+		}
+		
+		return true;
+	}
+	
+	public void iterate() {
+		HashSet<AbstractNode> visitedNodes = new HashSet<AbstractNode>();
+		
+		if(result instanceof AbstractContainerNode) {
+			AbstractContainerNode node = (AbstractContainerNode) result;
+			visitContainerNode(node, visitedNodes);
+		}
+	}
+}
---
+++ src/main/java/gtd/result/NodeStringifier.java
@@ -0,0 +1,52 @@
+package gtd.result;
+
+import gtd.result.struct.Link;
+import gtd.util.ArrayList;
+
+import java.util.HashSet;
+
+@SuppressWarnings("unchecked")
+public class NodeStringifier {
+	
+	public static boolean evalPrefix(Link link, StringBuilder sb, HashSet<Link> visitedLinks) {
+		if(link == null) return true;
+		
+		if(!visitedLinks.add(link)) return false;
+		
+		if(link.prefixes != null) {
+			ArrayList<Link> prefixes = link.prefixes;
+			for(int i = prefixes.size() - 1; i >= 0; --i) {
+				if(evalPrefix(prefixes.get(i), sb, visitedLinks)) break;
+			}
+		}
+		
+		evalNode(link.node, sb, visitedLinks);
+		return true;
+	}
+	
+	public static void evalNode(AbstractNode node, StringBuilder sb, HashSet<Link> visitedLinks) {
+		if(node instanceof AbstractContainerNode) {
+			Object alternatives = ((AbstractContainerNode) node).alternatives;
+			Link alternative;
+			if(alternatives instanceof Link){
+				alternative = (Link) alternatives;
+			}else{
+				alternative = ((ArrayList<Link>) alternatives).get(0);
+			}
+			evalPrefix(alternative, sb, visitedLinks);
+		} else if(node instanceof LiteralNode) {
+			sb.append(((LiteralNode) node).content);
+		} else if(node instanceof CharNode) {
+			sb.append(((CharNode) node).character);
+		}
+	}
+	
+	public static String nodeToString(AbstractNode node) {
+		StringBuilder sb = new StringBuilder();
+		HashSet<Link> visitedLinks = new HashSet<Link>();
+		
+		evalNode(node, sb, visitedLinks);
+		
+		return sb.toString();
+	}
+}
